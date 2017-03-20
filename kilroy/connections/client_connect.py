9bd74b60fe00d053a395dbfd0ad3dc02c88c86c6
PseudoDesign <pseudodesign@users.noreply.github.com>
2017-03-20 18:33:10 +0000

    test_can_connect is working

--- kilroy/connections/client_connect.py
+++ kilroy/connections/client_connect.py
@@ -1,3 +1,10 @@
+import asyncio
+
+
 class Connection:
     def __init__(self, **kwargs):
         pass
+
+    async def await_until_connected(self):
+        await asyncio.sleep(.2)
+        return False
--- kilroy/tests/test_connections.py
+++ kilroy/tests/test_connections.py
@@ -1,6 +1,7 @@
 import unittest
 from kilroy import Connection, CLIENT_DISCORD
 import asyncio
+from concurrent.futures import FIRST_COMPLETED
 
 
 class TestDiscordConnection(unittest.TestCase):
@@ -9,4 +10,23 @@ class TestDiscordConnection(unittest.TestCase):
         cls.connection = Connection(client=CLIENT_DISCORD)
 
     def test_can_connect(self):
-        CONNECTION_TIMEOUT_SECONDS = 5
+        async def go():
+            CONNECTION_TIMEOUT_SECONDS = 5
+            future = self.connection.await_until_connected()
+            done, pending = await asyncio.wait(
+                [future],
+                timeout=CONNECTION_TIMEOUT_SECONDS,
+                return_when=FIRST_COMPLETED
+            )
+            for f in pending:
+                f.cancel()
+            self.assertEqual(
+                len(pending),
+                0,
+                "Connection did not complete within timeout"
+            )
+            self.assertTrue(done.pop().result())
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(go())
+        loop.close()
