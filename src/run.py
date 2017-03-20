d531a567fff8f22c5033a2071da6935869c72c0f
Adam <PseudoDesign@users.noreply.github.com>
2017-03-20 03:52:45 +0000

    updated bootstrap script and run.py

---
+++ src/run.py
@@ -0,0 +1,33 @@
+import discord
+import asyncio
+import yaml
+
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await client.send_message(message.channel,
+                                        'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
+
+keys = yaml.load(open("../.keys.yaml", 'r'))
+client.run(keys['client_oauth_token'])
--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -2,4 +2,4 @@
 
 apt-get update
 apt-get install -y python3 python3-pip git
-pip3 install discord.py
+pip3 install discord.py pyyaml
