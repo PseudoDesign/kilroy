b24164d87b022054019fcbe6d821d631c4f66d21
PseudoDesign <PseudoDesign@users.noreply.github.com>
2017-04-07 05:40:46 +0000

    Started on plugins and setup scripts

---
+++ scripts/setup_db.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DB_NAME='kilroy'
+
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+
+# Include the "parse_yaml" function
+. ${DIR}/parse_yaml.sh
+
+apt-get update
+
+# Install python packages
+python3.5 -m pip install sqlalchemy pyaml pymysql
+
+# Install MariaDB (debian jessie)
+apt-get install -y software-properties-common
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
+add-apt-repository 'deb [arch=amd64,i386] http://sfo1.mirrors.digitalocean.com/mariadb/repo/10.1/debian jessie main'
+apt-get update
+apt-get install -y mariadb-server
+# Generate/get our keys
+python3 ${DIR}/../evelib/Keys.py
+eval $(parse_yaml ${DIR}/../.keys.yaml "key_")
+if [ -z "$key_sql_user" ]; then
+    echo "Error: unable to get sql_user key"
+    exit 1
+fi
+if [ -z "$key_sql_remote_user" ]; then
+    echo "Error: unable to get the sql_remote_user key"
+    exit 1
+fi
+# Initalize our db
+echo "Enter your MariaDB root password: "
+read -s PASSWORD
+mysql -u root -p"${PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
+mysql -u root -p"${PASSWORD}" -e "CREATE USER IF NOT EXISTS 'sql_user'@'localhost' IDENTIFIED BY '${key_sql_user}';"
+mysql -u root -p"${PASSWORD}" -e "GRANT CREATE, INSERT, SELECT ON ${DB_NAME}.* TO 'sql_user'@'localhost' IDENTIFIED BY '${key_sql_user}';"
+
+
