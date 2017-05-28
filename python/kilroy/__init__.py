c409afde360588f693f5155506a4a89b4733d1dd
ams-tech <adam@m-85.com>
2017-05-28 00:03:44 -0600

    made setup file

--- python/kilroy/__init__.py
+++ python/kilroy/__init__.py
@@ -2,3 +2,20 @@ from .connections import *
 from .conf import *
 from .db import *
 from .user import *
+import sys
+
+
+def main():
+    COMMANDS = {
+    #    "make_config": (create_sample_config_file, "Create a sample config file."),
+    }
+    if len(sys.argv) <= 1:
+        print("Hello, kilroy")
+    else:
+        command = sys.argv[1]
+        if command in COMMANDS:
+            COMMANDS[command][0](*sys.argv[2:])
+        else:
+            print("Commands:")
+            for command in COMMANDS:
+                print(command + " - " + COMMANDS[command][1])
