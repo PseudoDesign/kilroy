684073cdab03937329e411ef04b54c239df484d1
ams-tech <adam@m-85.com>
2017-06-01 00:04:46 -0600

    reworked file structure

--- python/kilroy/__init__.py
+++ python/kilroy/__init__.py
@@ -3,56 +3,9 @@ from .conf import *
 from .db import *
 from .user import *
 from .plugin_api import *
+from .kilroy import *
 import sys
-import yaml
-from threading import Lock
 
-
-class Kilroy:
-
-    __AVAILABLE_CONNECTIONS = [
-        DiscordConnection,
-    ]
-
-    __AVAILABLE_PLUGINS = [
-        HelloKilroy,
-    ]
-
-    def __init__(self, conf_file=None):
-        """
-        A chatbot plugin API for multiple chat services.
-        :param conf_file: File path of a config .yaml file
-        """
-        self.__plugin_lock = Lock()
-        self.available_connections = {}
-        for a in self.__AVAILABLE_CONNECTIONS:
-            self.available_connections[a.CLIENT_NAME] = a
-        self.available_plugins = {}
-        for a in self.__AVAILABLE_PLUGINS:
-            self.available_plugins[a.PLUGIN_NAME] = a
-        self.connections = []
-        self.plugins = []
-        if conf_file is not None:
-            fpt = open(conf_file, 'r')
-            data = yaml.load(fpt)
-            fpt.close()
-
-            for c in data['connections']:
-                self.connections += [self.available_connections[c['client']](**c)]
-
-            for p in data['plugins']:
-                self.load_plugin(self.available_plugins[p['name']](**p))
-
-    def load_plugin(self, plugin):
-        """
-        :param plugin: A plugin class to install.
-        :type plugin: PluginApi
-        :return:
-        """
-        with self.__plugin_lock:
-            if plugin in self.plugins:
-                return None
-            self.plugins += [plugin]
 
 
 def main():
---
+++ python/kilroy/kilroy.py
@@ -0,0 +1,54 @@
+from threading import Lock
+from . import *
+import yaml
+from functools import partial
+
+
+class Kilroy:
+
+    __AVAILABLE_CONNECTIONS = [
+        DiscordConnection,
+    ]
+
+    __AVAILABLE_PLUGINS = [
+        HelloKilroy,
+    ]
+
+    def __init__(self, conf_file=None):
+        """
+        A chatbot plugin API for multiple chat services.
+        :param conf_file: File path of a config .yaml file
+        """
+        self.__plugin_lock = Lock()
+        self.available_connections = {}
+        for a in self.__AVAILABLE_CONNECTIONS:
+            self.available_connections[a.CLIENT_NAME] = a
+        self.available_plugins = {}
+        for a in self.__AVAILABLE_PLUGINS:
+            self.available_plugins[a.PLUGIN_NAME] = a
+        self.connections = []
+        self.plugins = []
+        if conf_file is not None:
+            fpt = open(conf_file, 'r')
+            data = yaml.load(fpt)
+            fpt.close()
+
+            for c in data['connections']:
+                connection = self.available_connections[c['client']](**c)
+                #connection.add_message_listener()
+                self.connections += [connection]
+
+            for p in data['plugins']:
+                self.load_plugin(self.available_plugins[p['name']](**p))
+
+    def load_plugin(self, plugin):
+        """
+        :param plugin: A plugin class to install.
+        :type plugin: PluginApi
+        :return:
+        """
+        with self.__plugin_lock:
+            if plugin in self.plugins:
+                return None
+            self.plugins += [plugin]
+
