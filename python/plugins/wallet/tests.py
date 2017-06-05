207ab4b40d807f17ffc492c372fad171501be5fd
PseudoDesign <PseudoDesign@users.noreply.github.com>
2017-06-04 22:40:44 -0600

    started tests kinda

--- python/plugins/wallet/tests.py
+++ python/plugins/wallet/tests.py
@@ -2,4 +2,9 @@ import unittest
 
 
 class TestWallet(unittest.TestCase):
-    pass
+    TEST_USER_ID = 2
+    TEST_USER_ID_2 = 5
+
+    def test_get_balance(self):
+        # Should be 0 since the user din't exist before now
+        pass
