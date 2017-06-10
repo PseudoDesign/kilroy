0448f319063d2140657aa86ffb773170c29786ae
PseudoDesign <PseudoDesign@users.noreply.github.com>
2017-06-10 15:15:10 -0600

    added aioconsole to bootstrap

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -6,4 +6,4 @@ apt-get install -y python3-pip git
 sudo add-apt-repository -y ppa:fkrull/deadsnakes
 apt-get update
 sudo apt-get install -y python3.5
-python3.5 -m pip install discord.py pyyaml squalchemy
+python3.5 -m pip install discord.py pyyaml squalchemy aioconsole
