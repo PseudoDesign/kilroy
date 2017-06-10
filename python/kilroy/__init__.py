3ca503beca6a6b3cda56db5327c41af9d9c4bf68
PseudoDesign <PseudoDesign@users.noreply.github.com>
2017-06-10 17:31:18 -0600

    Updated console

--- python/kilroy/__init__.py
+++ python/kilroy/__init__.py
@@ -4,20 +4,38 @@ from .db import *
 from .user import *
 from .plugin_api import *
 from .kilroy import *
+from .strings import *
 import sys
 from aioconsole import ainput
 
 
 def run_with_console(*args):
     async def console():
-        i = await ainput("Enter a command: ")
-        print(i)
+        i = ""
+        if "-q" not in args:
+            print(strings.CONSOLE_LICENSE_NOTICE)
+        while i != "quit":
+            i = await ainput("Enter a command: ")
+            i = i[:-1].lower()
+            if i in commands:
+                if commands[i] is not None:
+                    await commands[i]()
+            else:
+                print("'" + i + "' " + "is not a valid command.")
+        print("Shutting down...")
         await k.end_connections()
 
-    if len(args) > 0:
-        k = kilroy.Kilroy(args[0])
-    else:
-        k = kilroy.Kilroy()
+    conf_file = None
+    for a in args:
+        if not a.startswith("-"):
+            conf_file = a
+            break
+
+    k = kilroy.Kilroy(conf_file)
+
+    commands = {
+        "quit": None
+    }
 
     tasks = [
         k.loop.create_task(console()),
@@ -26,18 +44,22 @@ def run_with_console(*args):
     k.unload()
 
 
+def print_commands(commands, *args):
+    print("Commands:")
+    for command in commands:
+        print(command + " - " + commands[command][1])
+
 
 def main():
     COMMANDS = {
         "console": (run_with_console, "Start's a Kilroy connection with a console.  Accepts a config file."),
     }
+
     if len(sys.argv) <= 1:
-        print("Hello, kilroy")
+        print_commands(COMMANDS)
     else:
         command = sys.argv[1]
         if command in COMMANDS:
             COMMANDS[command][0](*sys.argv[2:])
         else:
-            print("Commands:")
-            for command in COMMANDS:
-                print(command + " - " + COMMANDS[command][1])
+            print_commands(COMMANDS)
---
+++ python/kilroy/strings.py
@@ -0,0 +1,12 @@
+VERSION = "0.0.1d"
+
+PROGRAM_NAME = "kilroy"
+COPYRIGHT_YEAR = "2017"
+PROGRAM_AUTHOR = "Pseudo Design, LLC"
+
+CONSOLE_LICENSE_NOTICE = '''
+    {0} version {3}, Copyright (C) {2} {1}
+    {0} comes with ABSOLUTELY NO WARRANTY.
+    This is free software, and you are welcome to redistribute it
+    under the conditions of the GPLv2 license.
+'''.format(PROGRAM_NAME, PROGRAM_AUTHOR, COPYRIGHT_YEAR, VERSION)
