a8832111a7bb1464a7d5a4a5d4637b8cb090da57
PseudoDesign <PseudoDesign@users.noreply.github.com>
2017-06-12 19:58:33 -0600

    changed users to bigint

--- python/kilroy/db/user.py
+++ python/kilroy/db/user.py
@@ -1,11 +1,16 @@
 from . import SqlBase
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, BigInteger
 from .object import SqlObjectInterface
+from sqlalchemy.dialects import sqlite
+
+
+# sqlite doesn't support BigInteger auto-increment primary keys.  Map it to Integer, just for testing
+BigInt = BigInteger().with_variant(sqlite.INTEGER(), 'sqlite')
 
 
 class DbUser(SqlBase, SqlObjectInterface):
     __tablename__ = "user"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(BigInt, primary_key=True)
     client_name = Column(String(128), nullable=False)
     client_id = Column(String(128), nullable=False)
--- python/plugins/wallet/db_objects.py
+++ python/plugins/wallet/db_objects.py
@@ -4,11 +4,12 @@ from sqlalchemy import Column, Integer, ForeignKey, BigInteger
 from sqlalchemy.dialects import sqlite
 
 
+# sqlite doesn't support BigInteger auto-increment primary keys.  Map it to Integer, just for testing
+BigInt = BigInteger().with_variant(sqlite.INTEGER(), 'sqlite')
+
+
 class UserBalance(SqlBase, SqlObjectInterface):
     __tablename__ = "wallet_balance"
 
-    # sqlite doesn't support BigInteger auto-increment primary keys.  Map it to Integer, just for testing
-    BigInt = BigInteger().with_variant(sqlite.INTEGER(), 'sqlite')
-
-    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    user_id = Column(BigInt, ForeignKey('user.id'), primary_key=True)
     balance = Column(BigInt, default=0)
