e140d73227c68fe6639f8f690d83aab785ec80d0
PseudoDesign <PseudoDesign@users.noreply.github.com>
2017-06-12 21:38:41 -0600

    made transaction object

--- python/plugins/wallet/db_objects.py
+++ python/plugins/wallet/db_objects.py
@@ -1,7 +1,8 @@
 from kilroy.db import SqlBase
 from kilroy.db.object import SqlObjectInterface
-from sqlalchemy import Column, Integer, ForeignKey, BigInteger
+from sqlalchemy import Column, ForeignKey, BigInteger, DateTime
 from sqlalchemy.dialects import sqlite
+from datetime import datetime
 
 
 # sqlite doesn't support BigInteger auto-increment primary keys.  Map it to Integer, just for testing
@@ -12,4 +13,14 @@ class UserBalance(SqlBase, SqlObjectInterface):
     __tablename__ = "wallet_balance"
 
     user_id = Column(BigInt, ForeignKey('user.id'), primary_key=True)
-    balance = Column(BigInt, default=0)
+    balance = Column(BigInt, default=0, nullable=False)
+
+
+class Transaction(SqlBase, SqlObjectInterface):
+    __tablename__ = "wallet_transaction"
+
+    id = Column(BigInt, primary_key=True, autoincrement=True)
+    source_user = Column(BigInt, ForeignKey('user.id'), nullable=False)
+    destination_user = Column(BigInt, ForeignKey('user.id'), nullable=False)
+    amount = Column(BigInt, nullable=False)
+    timestamp = Column(DateTime, nullable=False, default=datetime.now)
